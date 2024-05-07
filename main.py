eef3b679620721e43649d8ea98b98e345b170436
mitya298 <mitya298@gmail.com>
2024-05-07 14:31:17 +0300

    add input func to main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+# comments
 if __name__ == '__main__':
     print('Game_over')
-
-
+    input("\n\n Нажмите ENTER, чтобы выйти...")
