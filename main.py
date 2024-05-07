045e539b7cb41a3dc736be60b4f8ca6d5d840aa1
mitya298 <mitya298@gmail.com>
2024-05-07 15:43:49 +0300

    correct func game_over, add game_over2 func  to main.py

--- main.py
+++ main.py
@@ -3,9 +3,37 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-# comments
-if __name__ == '__main__':
+# Chapter 1
+def game_over():
     print('Game_over')
     input("\n\n Нажмите ENTER, чтобы выйти...")
     print("\nпломбир")
     print("Доусон М\nПрограммирование на Python")
+
+# Chapter 2
+
+# Game Over 2.0
+# показывает разыне приёмы работы со строками
+
+def game_over_2_0():
+    print("Программа 'Game Over 2.0'")
+    print("То же", "самое", "сообщение")
+    print("Только",
+          "чуть-чуть",
+          "побольше")
+    print("Вот", end=" ")
+    print("оно...")
+    print(
+        """
+         _____
+        /
+        |
+        |
+        |
+        \\______
+        """)
+    input("\n\n Нажмите Enter, чтобы выйти...")
+
+if __name__ == '__main__':
+    #game_over()
+    game_over_2_0()
