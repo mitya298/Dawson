e74b592a6014675bf1dcef4959b198b18149bc3a
mitya298 <mitya298@gmail.com>
2024-05-07 21:25:46 +0300

    add chapter package

--- main.py
+++ main.py
@@ -1,37 +1,10 @@
 # This is a sample Python script.
-from chapter_1 import game_over
-
+from chapters import chapter_1
+from chapters import chapter_2
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-
-
-# Chapter 2
-
-# Game Over 2.0
-# показывает разыне приёмы работы со строками
-
-def game_over_2_0():
-    print("Программа 'Game Over 2.0'")
-    print("То же", "самое", "сообщение")
-    print("Только",
-          "чуть-чуть",
-          "побольше")
-    print("Вот", end=" ")
-    print("оно...")
-    print(
-        """
-         _____
-        /
-        |
-        |
-        |
-        \\______
-        """)
-    input("\n\n Нажмите Enter, чтобы выйти...")
-
 if __name__ == '__main__':
-    game_over()
-    game_over_2_0()
+    chapter_1.game_over()
+    chapter_2.game_over_2_0()
