08bd07a2b8cae64d1cb19f76f48be1ed5d18e9d4
mitya298 <mitya298@gmail.com>
2024-05-12 18:59:02 +0300

    add silly_strings func in chapter_2.py corrected fancy_credits func in chapter.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from chapters import chapter_2
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 if __name__ == '__main__':
-    chapter_1.game_over()
-    chapter_2.game_over_2_0()
-    chapter_2.fancy_credits()
+    #chapter_1.game_over()
+    #chapter_2.game_over_2_0()
+    #chapter_2.fancy_credits()
+    chapter_2.silly_strings()
